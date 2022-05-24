07eac36872af03fc32278fd43b0bc58cb97ad11b
zhoup <1025658085@qq.com>
2022-05-24 22:58:35 +0800

    524

--- generator/template/config/config.js
+++ generator/template/config/config.js
@@ -1,5 +1,25 @@
 
 export default defineConfig({
-  hash: true,
-  antd: {}
+  delopMode: 'assets',
+  applyType: 'console',
+  hash: true, // 按package.json 生成版本号
+  antd: {},
+  qiankun: {
+
+  },
+  publicPath: '/',
+  themeConfig: {
+
+  },
+  msfu: {
+    esbuild: true,
+    SSG: false,
+    SSR: false,
+    remoteRuntime: true
+  },
+  vite: false,
+  isForceLint: true,
+  tract: {
+    a: '1123'
+  }
 });
